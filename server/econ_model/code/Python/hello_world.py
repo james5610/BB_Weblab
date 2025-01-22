d558b04f7dc330d70e3062efa6695ec048ded053
james5610 <jameslee5610@gmail.com>
2025-01-22 18:28:54 -0500

    removed unecessary files from econ_model

--- server/econ_model/code/Python/hello_world.py
+++
@@ -1,8 +0,0 @@
-# Specify the file name
-file_name = "hello_world.txt"
-
-# Open the file in write mode and save "hello world"
-with open(file_name, "w") as file:
-    file.write("hello world")
-
-print(f'"hello world" has been written to {file_name}')
